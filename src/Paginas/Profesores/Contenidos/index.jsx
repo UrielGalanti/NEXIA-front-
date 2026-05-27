4f2d2e814f96369d50a7489050cfbf54ef79b263
FELIPE MANUEL ANDRACA <48803376@ort>
2026-05-27 10:04:40 -0300

    contenidos

--- src/Paginas/Profesores/Contenidos/index.jsx
+++ src/Paginas/Profesores/Contenidos/index.jsx
@@ -3,14 +3,14 @@ import Sidebar from '../../../componentes/alumnos/SideBar'
 import Footer from '../../../componentes/footer'
 import Contenido from '../../../componentes/profesor/Contenido'
 
-function MisCursos() {
+function Contenidos() {
   const [trabajos, setTrabajos] = useState([
   ])
   return (
     <>
     <Sidebar/>
       <section>
-        <h1>Mis Cursos</h1>
+        <h1>Contenidos</h1>
           <div className="materias-grid">
           {trabajos.map((item) => (
             <Contenido titulo={item.titulo} descripcion={item.descripcion} tipo={item.tipo}
@@ -23,4 +23,4 @@ function MisCursos() {
   )
 }
 
-export default MisCursos
+export default Contenidos
